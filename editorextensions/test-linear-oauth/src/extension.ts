0e3400e97acdefc95dd4d863d9bebc39cac851a2
Austin Galura <agalura@lucidchart.com>
2023-11-27 09:34:04 -0700

    oops

--- editorextensions/test-linear-oauth/src/extension.ts
+++ editorextensions/test-linear-oauth/src/extension.ts
@@ -5,17 +5,12 @@ import { LinearClient } from '@linear/sdk';
 const client = new EditorClient();
 const menu = new Menu(client);
 const viewport = new Viewport(client);
-// const client2 = new LinearClient({
-//     accessToken: "lin_oauth_c24bcce279969d7e8c9170513f57110119a6418c603e233ebbbdcb8ea1e2477f"
-//   });
-
 client.registerAction('test', () => {
     const modal = new ImportModal(client);
     modal.show();
 });
 
 client.registerAction('testRequest', async () => {
-    // client2.issues();
     client.oauthXhr("linear", {
         url: "https://api.linear.app/graphql",
         method: "POST",
